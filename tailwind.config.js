46b26d8c45197b205e740d36f1153c0b211d1262
ivo.tsanov <ivo.tsanov@securax.org>
2026-01-12 11:31:54 +0200

    Tailwind config fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 export default {
-  content: ['./index.html', '@/**/*.{js,jsx}'],
-  theme: {extend: {}},
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
